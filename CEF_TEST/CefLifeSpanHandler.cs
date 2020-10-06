7b3d6ad17fc36392f9c3f2ca2e54b7e441543b49
Choi Yang Hwan <choiyanghwan@wemakeprice.com>
2020-10-06 17:01:49 +0900

    처음

---
+++ CEF_TEST/CefLifeSpanHandler.cs
@@ -0,0 +1,41 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CEF_TEST
+{
+    class CefLifeSpanHandler : ILifeSpanHandler
+    {
+        public event Action<string> popup_request;
+        
+        public bool DoClose(IWebBrowser browserControl, IBrowser browser)
+        {
+            if (browser.IsDisposed || browser.IsPopup)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void OnAfterCreated(IWebBrowser browserControl, IBrowser browser)
+        {            
+        }
+
+        public void OnBeforeClose(IWebBrowser browserControl, IBrowser browser)
+        {            
+        }
+
+        public bool OnBeforePopup(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            if (popup_request != null)
+            {
+                popup_request(targetUrl);
+            }
+            // browser.MainFrame.LoadUrl(targetUrl);
+            newBrowser = null;
+            return true;
+        }
+    }
+}
---
+++ CEF_TEST/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CEF_TEST
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ChromiumWebBrowser> Tabs { get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CefSettings settings = new CefSettings
+            {
+                RemoteDebuggingPort = 8088
+            };
+
+            settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF";
+            settings.CefCommandLineArgs.Add("disable-usb-keyboard-detect", "1");
+            settings.CefCommandLineArgs.Add("persist_session_cookies", "1");
+            settings.CefCommandLineArgs.Add("disable-gpu-vsync", "1");
+            settings.CefCommandLineArgs.Add("disable-gpu", "1");
+
+            Cef.Initialize(settings);
+
+            #region Tab Control 
+            //Tabs = Enumerable.Range(1, 2).Select(x => new ChromiumWebBrowser
+            //{
+            //    Address = "google.com"
+            //}).ToList();
+
+            //ChromiumWebBrowser abc = new ChromiumWebBrowser();            
+            //ChromiumWebBrowser ab = new ChromiumWebBrowser();
+            //Tabs = new List<ChromiumWebBrowser>();
+            //Tabs.Add(abc);
+            //Tabs.Add(ab);
+
+            //abc.Load("https://www.google.com");
+            //ab.Load("https://www.google.com");
+
+            var tt = Environment.CurrentDirectory;
+            CefLifeSpanHandler cefLifeSpanHandler = new CefLifeSpanHandler();
+            cefLifeSpanHandler.popup_request += Life_popup_request;
+
+            Tabs = new ObservableCollection<ChromiumWebBrowser>();
+            Tabs.Add(new ChromiumWebBrowser() { Address = tt + @"\test.html", LifeSpanHandler = cefLifeSpanHandler });
+
+            DataContext = this;
+            #endregion
+        }
+
+        private void Life_popup_request(string obj)
+        {
+            //function for open pop up in a new browser
+            Carregar_popup_new_browser(obj);
+        }
+
+        private void Carregar_popup_new_browser(string url)
+        {
+            //open pop up in second browser
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            {
+                Tabs.Add(new ChromiumWebBrowser() { Address = url });
+
+                tabcontrol.SelectedIndex = Tabs.Count - 1;
+                // splashScreen.Close();
+            }));
+
+            
+            // chrome_popup.Load(url);
+        }
+    }
+}
